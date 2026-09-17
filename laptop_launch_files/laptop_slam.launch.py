ca6f763e0fc81bb8c9d3058ba29eff81a261caf7
albertovic <alberto.vicentechacon@gmail.com>
2026-09-18 00:05:01 +0200

    Added the laptop launchfile to the repo

---
+++ laptop_launch_files/laptop_slam.launch.py
@@ -0,0 +1,37 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    # Find the official SLAM Toolbox launch file
+    slam_toolbox_dir = get_package_share_directory('slam_toolbox')
+    slam_launch_file = os.path.join(slam_toolbox_dir, 'launch', 'online_async_launch.py')
+    
+    # Path to the RViz config you just saved
+    rviz_config = os.path.expanduser('~/.rviz2/slam_config.rviz')
+    
+    return LaunchDescription([
+        # 1. Joystick Driver (Reads the controller plugged into the laptop)
+        Node(
+            package='joy',
+            executable='joy_node',
+            name='joy_node',
+            parameters=[{'deadzone': 0.05}]
+        ),
+        
+        # 2. SLAM Toolbox
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(slam_launch_file)
+        ),
+        
+        # 3. RViz2
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config]
+        )
+    ])
